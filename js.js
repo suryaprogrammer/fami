b1d1ef6e5172bd910b44e0260bd19854dd568957
surya <codewithsurya7@gmail.com>
2022-04-30 10:53:09 +0530

    box

--- js.js
+++ js.js
@@ -1,37 +1,46 @@
-let root = document.getElementById("root");
 
-for (let i = 0; i < 20; i++) {
-   
-    
-
-root.innerHTML +=`
-<div id="card">
-
-    <div id="inner_card1" class="inner_card">
-      
-    </div>
-    <div id="inner_card2" class="inner_card">
-        <h1>village !</h1>
-
-    </div>
-    <div id="inner_card3" class="inner_card">
-        <button type="button" class="btn btn-primary">Img</button>
-    <a href="#b">
-    <button onclick="vedio()" type="button" class="btn btn-secondary">Vedio</button></a>
-        <button class="btn"><i class="fa fa-download"></i> Download</button>
-    </div>
-</div>`;
-
-}
-
-function vedio(params) {
-    
-let vedio = document.getElementById("ve");
-
-    vedio.innerHTML=`<iframe width="560" height="315" src="https://www.youtube.com/embed/JLicILeylnk"  frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`;
-}
 
 
 function btn_dark(){
     
 }
+
+fetch("data.json").then(res => res.json())
+.then(data => {
+    let root = document.getElementById("root");
+
+    for (let i = 0; i < 20; i++) {
+       
+        
+    
+    root.innerHTML +=`
+    <div  id="card">
+    
+        <div id="inner_card1" class="inner_card">
+          <img id="img" src="${data[i].bgc}">
+        </div>
+        <div id="inner_card2" class="inner_card">
+            <h1>${data[i].topic}</h1>
+    
+        </div>
+        <div id="inner_card3" class="inner_card">
+           <a href="${data[i].img}"> <button type="button" class="btn btn-primary">Img</button></a>
+        <a href="#b">
+        <button onclick="vedio()" type="button" class="btn btn-secondary">Vedio</button></a>
+            <button class="btn"><i class="fa fa-download"></i> Download</button>
+        </div>
+    </div>`;
+    // return data;
+
+    }
+
+  
+});
+
+
+function vedio() {
+    
+    let vedio = document.getElementById("ve");
+    alert("hi");
+        vedio.innerHTML=`${data[1].link}`;
+    }
